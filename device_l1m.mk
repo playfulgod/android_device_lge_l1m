957b1b57159efb7aa6c966c48d812589837e1549
PlayfulGod <playfulgod@gmail.com>
2013-09-26 17:05:11 -0400

    deleted:    recovery/graphics.c

--- device_l1m.mk
+++ device_l1m.mk
@@ -1,19 +1,3 @@
-#
-# Copyright (C) 2011 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
@@ -27,6 +11,10 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/prebuilt/root/init.lge.usb.rc:root/init.lge.usb.rc \
 	$(LOCAL_PATH)/prebuilt/root/init.lge.usb.sh:root/init.lge.usb.sh \
 	$(LOCAL_PATH)/prebuilt/root/ueventd.l1m.rc:root/ueventd.l1m.rc
+
+# Recovery
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/recovery/fstab.l1m:recovery/root/fstab.l1m
 
 # OMX
 PRODUCT_PACKAGES += \
